0d045cc876dcffde530262453bb482f582cc753a
hmmnd <raymond-hammond@uiowa.edu>
2014-12-09 22:56:13 -0600

    Create IteratorPatternTest.java
    Start of testing

---
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/IteratorPatternTest.java
@@ -0,0 +1,14 @@
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IteratorPatternTest {
+
+	@Test //test that iterator returns all the values
+	public void test() {
+		
+	}
+
+}
