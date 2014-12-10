d78a6f9f3623f42c26ca819539017d7c503f6a2d
hmmnd <raymond-hammond@uiowa.edu>
2014-12-09 20:18:13 -0600

    Create Handler.java
    Iterator pattern

---
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Handler.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Raymond
+ *
+ */
+package org.uiowa.cs2820.engine;
+
+import java.util.*;
+
+//another part of the Iterator pattern
+public interface Handler {
+	public Iterator getIterator();
+}
