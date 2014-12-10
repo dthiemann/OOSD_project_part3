09788c27ebcdad0d77d0359eb8a18bfe3527eac6
Dylan Thiemann <dylanthiemann@gmail.com>
2014-12-09 19:51:47 -0600

    working on string parser

---
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/StringParserUtility.java
@@ -0,0 +1,32 @@
+package org.uiowa.cs2820.engine;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author dthiemann
+ * 
+ * This class is a utility that will parse a string into 
+ * its sub-queries and then return the individual queries
+ *
+ */
+public class StringParserUtility {
+	public static void parse() {
+		int count_open = getCharCount(equation, '(');
+		int count_close = getCharCount(equation, ')');
+
+		ArrayList<String> arguments = getQueryArguments(equation, count_open, count_close);
+		
+		int numberOfQueries = arguments.size()/3;
+		
+		ArrayList<String> combinators = getQueryCombinators(equation, numberOfQueries);
+
+		for (int i = 0; i < arguments.size(); i++) {
+			System.out.println(arguments.get(i));
+		}
+		
+		for(int i = 0; i < combinators.size(); i++) {
+			System.out.println(combinators.get(i));
+		}
+	}
+}
