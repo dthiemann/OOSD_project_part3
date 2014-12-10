2fb7e9120b5cfee2a7f03dc280877f3199b63b1b
hmmnd <raymond-hammond@uiowa.edu>
2014-12-09 20:19:37 -0600

    Create Iterator.java
    Iterator pattern

---
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Iterator.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Raymond
+ *
+ */
+package org.uiowa.cs2820.engine;
+
+import java.util.*;
+//This is part of the Iterator pattern that will 
+//return an iterator instead of array of strings
+public interface Iterator {
+	public boolean hasNext();
+	public Object next();
+}
