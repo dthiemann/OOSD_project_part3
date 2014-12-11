38718f100fd0df31b1dc5c41ab2f961175ba3d6e
Dylan Thiemann <dylanthiemann@gmail.com>
2014-12-10 20:05:29 -0600

    Added test skeletons for stringparser

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/StringParserUtilityTest.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/StringParserUtilityTest.java
@@ -1,5 +1,70 @@
 package org.uiowa.cs2820.engine;
 
-public class StringParserUtilityTest {
+import static org.junit.Assert.*;
+import org.junit.*;
 
+public class StringParserUtilityTest {
+	
+	/*
+	 * Test that the utility can handle a simple query
+	 */
+	@Test
+	public void test1() {
+		assertEquals(1,1);
+	}
+	
+	/*
+	 * Test that it sends the correct error when 
+	 * the keyword Search is missing
+	 */
+	@Test
+	public void test2() {
+		
+	}
+	
+	/*
+	 * Test that it sends the correct error when the
+	 * parentheses don't match up
+	 */
+	@Test
+	public void test3() {
+		
+	}
+	
+	/*
+	 * Test that it sends the correct error when there
+	 * is an empty query
+	 */
+	@Test
+	public void test4() {
+		
+	}
+	
+	/*
+	 * Test that it parses the string correctly
+	 */
+	
+	@Test
+	public void test5() {
+		
+	}
+	
+	/*
+	 * Test for proper handling of Null values
+	 */
+	
+	@Test
+	public void test6() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
