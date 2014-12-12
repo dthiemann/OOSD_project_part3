402b7f939e16f8e2ad765425c5a9a9a7d80a8ea1
hmmnd <raymond-hammond@uiowa.edu>
2014-12-11 22:48:55 -0600

    Cleaned imports and created line by line output

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/rServer.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/rServer.java
@@ -1,21 +1,10 @@
 package org.uiowa.cs2820.engine;
 
 
-	import java.util.List;
-import java.util.ArrayList;
-
-import javax.ws.rs.PUT;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-
-	import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONTokener;
-import org.codehaus.jettison.json.JSONException;
 
 	// The Java class will be hosted at the URI path "/rService
 	@Path("/rServer")
@@ -30,12 +19,15 @@ import org.codehaus.jettison.json.JSONException;
 	    public String[] getFields(Field req) {
 	    	FieldSearch A = new FieldSearch(req);
 	    	String D [] = A.findEquals();
-	    	System.out.println(D);
 	    	if (D.length < 3){
 	    		Indexer I = new Indexer("first.db");
 	    		I.addField(req);
 	    		I = new Indexer("second.db");
 	    		I.addField(req);
 	    	}
-	    	return D;	}
+	    	for (String elements : D) {
+	    		System.out.println(elements);
+	    		}
+	    	return D;	
+	    }
 }
