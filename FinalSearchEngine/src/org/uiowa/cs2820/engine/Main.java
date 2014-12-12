6284087fb04ac5d6b7a98a7e6776a775654ec192
hmmnd <raymond-hammond@uiowa.edu>
2014-12-11 23:52:37 -0600

    create main for server

---
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Main.java
@@ -0,0 +1,24 @@
+package org.uiowa.cs2820.engine;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Main {
+    public static final String BASE_URI = "/rServer";
+    public static HttpServer startServer() {
+        // this next line is used by Jersey to find classes that handle requests
+        final ResourceConfig rc = new ResourceConfig().packages("edu.iowa");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
+    }
+}
