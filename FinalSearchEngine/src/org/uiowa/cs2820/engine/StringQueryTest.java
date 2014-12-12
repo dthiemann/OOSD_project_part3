28d9009fe2a48b598c742c44720146f77010ff57
Dylan Thiemann <dylanthiemann@gmail.com>
2014-12-12 14:37:22 -0600

    added test taht shows that StringQuery performs the correct query

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/StringQueryTest.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/StringQueryTest.java
@@ -22,4 +22,25 @@ public class StringQueryTest {
 		
 		assert(results == null);
 	}
+	
+	/*
+	 * Test that the StringQuery class can actually return 
+	 * the correct results
+	 */
+	@Test
+	public void test2() {
+		CheckPoint.reset();
+		DiskSpace.reset();
+		Field f = new Field("test", "other_test");
+		Indexer I = new Indexer("my_file.txt");
+		I.addField(f);
+		
+		String equation = "Search (equals (test, other_test))";
+		
+		ParsedString myPS = StringParserUtility.parse(equation);
+		
+		StringQuery query = new StringQuery(myPS);
+		String[] results = query.executeQueries();
+		assert(results[0].equals("my_file.txt"));
+	}
 }
