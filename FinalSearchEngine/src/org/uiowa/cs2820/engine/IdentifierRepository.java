ba0b17b8ba39092f0800afb1acab061beebb09ac
hmmnd <raymond-hammond@uiowa.edu>
2014-12-12 12:43:30 -0600

    Update IdentifierRepository.java

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/IdentifierRepository.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/IdentifierRepository.java
@@ -5,34 +5,29 @@
  */
 package org.uiowa.cs2820.engine;
 
-import java.util.*;
-
 //Iterator pattern requires that this implements the Handler
 public class IdentifierRepository implements Handler{
-
-	public String ids[] ={}; //function that gets all the ids in array list
-	//need something to fill in this blank
 	
-	@Override
+	private String[] ids;
+	
 	public Iterator getIterator() {
+		
 		return new IdIterator();
 	}
 	
 	private class IdIterator implements Iterator {
-		int index;
+		private int pos = 0;
 
-		@Override
 		public boolean hasNext() {
-			if (index < ids.length){
+			if (pos < ids.length){
 				return true;
 			}
 			return false;
 		}
 
-		@Override
 		public Object next() {
 			if (this.hasNext()){
-				return  ids[index++];
+				return  ids[pos++];
 			}
 			return null;
 		} 
