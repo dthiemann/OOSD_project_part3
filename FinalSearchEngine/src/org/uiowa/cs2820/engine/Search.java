5940ccc94a44e12e15209dfadd5a68d6e9591671
kgomez09 <karla-gomez@uiowa.edu>
2014-12-12 18:03:25 -0600

    Update Search.java

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/Search.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Search.java
@@ -1,14 +1,21 @@
 package org.uiowa.cs2820.engine;
 
 public class Search {
-		Field target;
-		Database D;
+	Field target;
+	Database D;
 	public Search(Field f) { 
-				D = new LinearDiskDatabase();
-				target = f; 
+		D = new LinearDiskDatabase();
+		target = f; 
 				}
 	public String[] find() {
 		String [] R = D.fetch(target);
 		if (R == null) R = new String[0];
 		return R;
 				}
+	@Override
+	public void Search(Field f) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
