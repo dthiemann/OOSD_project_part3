be762b027839b5a5486833740d2805f517443813
kgomez09 <karla-gomez@uiowa.edu>
2014-12-13 23:03:29 -0600

    Update Contains.java

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/Contains.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Contains.java
@@ -14,6 +14,9 @@ public class Contains {
 		if (R == null) R = new String[0];
 			return R;
 				}
+	/*
+	* Returns an ArrayList containing fields that had the same value as the Field target
+	*/
 	public ArrayList<String> GetFields(){
 		ArrayList<String> A = SearchContains.GetClst();
 		return A;
