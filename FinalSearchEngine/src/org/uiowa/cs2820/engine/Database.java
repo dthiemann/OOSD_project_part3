d960c196cf867140c0d60713d4aa258f44fe3fa6
kgomez09 <karla-gomez@uiowa.edu>
2014-12-13 18:40:53 -0600

    added fetchLess

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/Database.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Database.java
@@ -5,4 +5,6 @@ public interface Database {
 	  public abstract void store(Field f, String id); // store a Field/id
 	  public abstract void remove(String id); // delete document (by identifier) 
 	  public abstract String[] fetchGreater(Field f); 
+	  public abstract String[] fetchLess(Field F);
+	  
 	  }
