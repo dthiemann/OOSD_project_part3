239118de62aea1771f946ac709404a003bb323e9
kgomez09 <karla-gomez@uiowa.edu>
2014-12-13 18:10:46 -0600

    Update GreaterThan.java

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/GreaterThan.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/GreaterThan.java
@@ -6,17 +6,14 @@ public class GreaterThan {
 		target = f; 
 					}
 	public String[] find() {
-		String [] R = D.fetchOther(target);
+		String [] R = D.fetchGreater(target);
 		if (R == null) R = new String[0];
-				return R;
+			return R;
+				
 					}
-		}
 	public Object GetNodes(){
-		ArrayList<Nodes> A = SearchGreaterThan.GetList();
-		List<Nodes> B =A;
-		//**Iterator<node> iterator = B.iterator();
-		return B;	
-			
-	}
+		ArrayList<String> A = SearchGreaterThan.GetList();
+			return A;
+		}
 	
 }
