c51b151f1dcf4909d6ca2e493a1a7f670ddca46c
kgomez09 <karla-gomez@uiowa.edu>
2014-12-13 21:27:40 -0600

    Update Contains.java

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/Contains.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/Contains.java
@@ -6,21 +6,16 @@ public class Contains {
 	Field target;
 	Database D;
 	public Contains(Field f) { 
-				D = new LinearDiskDatabase();
-				target = f; 
+		D = new LinearDiskDatabase();
+		target = f; 
 				}
 	public String[] find() {
-		
-			System.out.println(target);
-			String [] R = D.fetchContains(target);
-		
-			if (R == null) R = new String[0];
-				return R;
-			
+		String [] R = D.fetchContains(target);
+		if (R == null) R = new String[0];
+			return R;
 				}
 	public ArrayList<String> GetFields(){
 		ArrayList<String> A = SearchContains.GetClst();
-		
 		return A;
 	}
 }
