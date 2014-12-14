9c3d41a05d6699b71fc486e78c50de2e3116db46
kgomez09 <karla-gomez@uiowa.edu>
2014-12-13 18:31:45 -0600

    Update LinearDiskDatabase.java

--- FinalSearchEngine/src/org/uiowa/cs2820/engine/LinearDiskDatabase.java
+++ FinalSearchEngine/src/org/uiowa/cs2820/engine/LinearDiskDatabase.java
@@ -16,9 +16,14 @@ public class LinearDiskDatabase implements Database {
 	    return Identifier.getAllIds(n.identlist);
 	    }
 	 public String[] fetchGreater(Field f) {
-	    Node n = Node.findNodeOthers(f);
+	    Node n = Node.findNodeGreater(f);
 	    if (n == null) return new String[0];
 	    return Identifier.getAllIds(n.identlist);
 	    }
+	 public String[] fetchLess(Field f){
+	    Node n = Node.findNodeLess(f);
+	    if (n == null) return new String[0];
+	    return Identifier.getAllIds(n.identlist);
+	 }
 	  }
 		  
